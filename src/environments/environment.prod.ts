6b95a7d8daf14ec304e7d722fdc2246035468f7e
Leah Gakii <leahgakii@gmail.com>
2019-04-01 15:56:00 +0300

    src/environments/environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
-export const environment = {
-  production: true,
-  apiurl:"https://api.github.com/users/Le07ah57532538?access_token =4af120d6cc58e9f5e1d8fc7d97d2b42d6515ff87"
+// export const environment = {
+//   production: true,
+//   apiurl:"https://api.github.com/users/Le07ah57532538?access_token =4af120d6cc58e9f5e1d8fc7d97d2b42d6515ff87"
 
-};
+// };
